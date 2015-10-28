e63b3e8267bdd8fe5592eead1ac81ce932b559fe
Daniel Huang <danielhuang@g.ucla.edu>
2015-10-27 22:50:09 -0700

    new homepage skeleton

--- js/intro.js
+++ js/intro.js
@@ -2,51 +2,35 @@
  * Created by Daniel on 8/31/2015.
  */
 jQuery(document).ready(function(){
-
-//    htmlCourse();
-    var introText=$(".element");
-    var outro=$('.outro');
-    introText.typed({
-        strings: ["} Coder.Central();"],
-        // typing speed
-        typeSpeed: 80,
-        // time before typing starts
-        startDelay: 400,
-        loopCount: false,
-        // show cursor
-        showCursor: true,
-        callback:function(){
-            $('.typed-cursor').fadeOut('fast');
-        }
+    $("#banner a").bind("click",function(event){
+        event.preventDefault();
+        var target = $(this).attr("href");
+        $("html, body").stop().animate({
+            scrollLeft: $(target).offset().left,
+            scrollTop: $(target).offset().top }, 1200);
     });
-
-    var winHeight= $(window).height();
-    //var winWidth= $(window).width();
-    var introHeight=(winHeight/2-100);
-    $('#intro').css({
-        'top': introHeight+'px'
-    });
-    setTimeout(function(){
-        introText.fadeOut('medium');
-        outro.typed({
-            strings: ["> Be Inspired."],
-            // typing speed
-            typeSpeed: 80,
-            // time before typing starts
-            startDelay: 300,
-            loopCount: false,
-            // show cursor
-            showCursor: true,
-            callback: function() {
-                setTimeout(function(){
-                    $('#intro').fadeOut();
-                },800);
-                setTimeout(function(){
-                    window.location = "homepage.html";
-                },1000);
-            }
-        });
-    },3500);
+    /*
+        setTimeout(function(){
+            introText.fadeOut('medium');
+            outro.typed({
+                strings: ["> Be Inspired."],
+                // typing speed
+                typeSpeed: 80,
+                // time before typing starts
+                startDelay: 300,
+                loopCount: false,
+                // show cursor
+                showCursor: true,
+                callback: function() {
+                    setTimeout(function(){
+                        $('#intro').fadeOut();
+                    },800);
+                    setTimeout(function(){
+                        window.location = "homepage.html";
+                    },1000);
+                }
+            });
+        },3500);*/
 
 });
 
