8f99d034db30a43c8891d0736bd09039d842f5a2
Daniel Huang <danielhuang@g.ucla.edu>
2015-10-30 01:10:06 -0700

    swiping on mobile :D

--- js/intro.js
+++ js/intro.js
@@ -2,21 +2,12 @@
  * Created by Daniel on 8/31/2015.
  */
 jQuery(document).ready(function(){
-
-    //temperary nav bar stuff
-    $("#banner a").bind("click",function(event){
-        event.preventDefault();
-        var target = $(this).attr("href");
-        $("html, body").stop().animate({
-            scrollLeft: $(target).offset().left,
-            scrollTop: $(target).offset().top }, 1200);
-    });
-
+    var pg=$(".page");
     //allows all arrows to scroll to certain sections of the page
     var body=$('body');
     var width=$(window).width();
     body.css({'width':width*4.4+'px'});
-    $('.page').css({'width':width*1.013+'px'});
+    pg.css({'width':width*1.013+'px'});
     var scroll=$('.scroll');
     scroll.bind("click",function(event){
         event.preventDefault();
@@ -25,12 +16,13 @@ jQuery(document).ready(function(){
             scrollLeft: $(target).offset().left,
             scrollTop: $(target).offset().top }, 1200);
     });
-    $('.page').swipe( {
+    var home=$('#home');
+    pg.swipe( {
         //Generic swipe handler for all directions
         swipe:function(event, direction, distance, duration) {
             var currentElement = this.next();
             var lastEl=this.prev();
-            if(this[0] != $('#home')[0]){
+            if(this[0] != home[0]){
                 if(direction=="left"){
                     $('html, body').animate({scrollLeft: $(currentElement).offset().left}, 800);
                 }else if( direction=="right"){
@@ -38,7 +30,6 @@ jQuery(document).ready(function(){
                         $('html, body').animate({scrollLeft: 0}, 800);
                     else
                         $('html, body').animate({scrollLeft: $(lastEl).offset().left}, 800);
-
                 }
             }else{
                     $('html, body').animate({scrollLeft: width*1.013}, 800);
@@ -48,7 +39,7 @@ jQuery(document).ready(function(){
         //Default is 75px, set to 0 for demo so any distance triggers swipe
         threshold:0
     });
-    
+
     //things to do on scroll if not on mobile
     if($(window).width()>400){
         $(window).on('scroll',function(){
@@ -80,27 +71,34 @@ jQuery(document).ready(function(){
 
 
     $('#title').delay(800).fadeIn(1500);
-    $('#slogan').typed({
-        strings: [">Learning Powered by Curiosity"],
-        // typing speed
-        typeSpeed: 10,
-        // time before typing starts
-        startDelay: 2000,
-        loopCount: false,
-        // show cursor
-        showCursor: false,
-        callback:function(){
-            $('.text-stuff').fadeIn(1000);
+    //add typing animation on desktops
+    if($(window).width()>400){
+        $('#slogan').typed({
+            strings: [">Learning Powered by Curiosity"],
+            // typing speed
+            typeSpeed: 10,
+            // time before typing starts
+            startDelay: 2000,
+            loopCount: false,
+            // show cursor
+            showCursor: false,
+            callback:function(){
+                $('.text-stuff').fadeIn(1000);
+                $('#real-content').fadeIn(1000);
+                $('#landing-nav').animate({
+                    'opacity':1,
+                    'right':40+'px'
+                },2000);
+            }
+        });
+    }else{
+        setTimeout(function(){$('.text-stuff').fadeIn(1000);
             $('#real-content').fadeIn(1000);
             $('#landing-nav').animate({
                 'opacity':1,
                 'right':40+'px'
-            },2000);
-        }
-    });
-
-
-
+            },2000);},1500);
+    }
 });
 
 
